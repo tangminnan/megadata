00bdb7107d9e4f8d9dab3bb750880625dc87818c
jiazhifang000 <jzf18235182968@163.com>
2021-01-07 13:02:05 +0800

    矫正第一次提交

---
+++ xinshinengdb/xinshineng-manager/src/main/java/com/xinshineng/information/controller/correctController.java
@@ -0,0 +1,33 @@
+package com.xinshineng.information.controller;
+
+import com.xinshineng.information.service.shaicha.service.schoolService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/skip")
+public class correctController {
+    @Autowired
+    private schoolService schoolservice;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @GetMapping("/Newhuyan")
+    public String Newhuyan(){
+        return "Newhuyan";
+    }
+
+    @GetMapping("/Newzidingyi")
+    public String Newzidingyi(){
+        return "Newzidingyi";
+    }
+
+
+}
